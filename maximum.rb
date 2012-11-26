17459db73ad7f6ac45d095097530d2fa8d4c4bbf
user <user@test.ru>
2012-11-26 19:15:04 +0400

    Initial commit

--- maximum.rb
+++ maximum.rb
@@ -1,11 +1,11 @@
-puts "Enter value for a:"
-a=gets
-puts "Enter value for b:"
-b=gets
+print "Enter value for a:"
+a=gets.delete("\n")
+print "Enter value for b:"
+b=gets.delete("\n")
 if a>b
-puts "Chislo #{a} bolshe #{b}"
+print "Chislo #{a} bolshe #{b}"
 elsif a==b
-puts "Chislo #{a} ravno #{b}"
+print "Chislo #{a} ravno #{b}"
 else
-puts "Chislo #{b} bolshe #{a}"
+print "Chislo #{b} bolshe #{a}"
 end
