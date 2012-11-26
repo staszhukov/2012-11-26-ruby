6faee585a7a8b446c64bf5acdbe28bbc4575af68
user <user@test.ru>
2012-11-26 18:16:07 +0400

    Initial commit

---
+++ maximum.rb
@@ -0,0 +1,11 @@
+puts "Enter value for a:"
+a=gets
+puts "Enter value for b:"
+b=gets
+if a>b
+puts "Chislo #{a} bolshe #{b}"
+elsif a==b
+puts "Chislo #{a} ravno #{b}"
+else
+puts "Chislo #{b} bolshe #{a}"
+end
