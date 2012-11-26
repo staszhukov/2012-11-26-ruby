becea4707596a8e00fab8f4de4b6b2b136244a8f
user <user@test.ru>
2012-11-26 19:36:44 +0400

    Initial commit

--- maximum.rb
+++ maximum.rb
@@ -1,8 +1,8 @@
 print "Enter value for a:"
-a=gets.delete("\n")
+a = gets.delete("\n")
 print "Enter value for b:"
-b=gets.delete("\n")
-if a>b
+b = gets.delete("\n")
+if a > b
 print "Chislo #{a} bolshe #{b}"
 elsif a==b
 print "Chislo #{a} ravno #{b}"
